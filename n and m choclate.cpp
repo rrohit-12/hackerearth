afca9d49042d73d2e0a28e879392ce9c0f00181d
Rony Rajput <67194882+rrohit-12@users.noreply.github.com>
2020-08-18 02:07:06 +0530

    Add files via upload
    n and m choclate problem using modulo funC

---
+++ n and m choclate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N,M,Q,i;
+	cin>>Q;
+	for(i=1;i<=Q;i++)	
+	{
+		cin>>N>>M;	
+	if(M%N==0)
+	{
+		cout<<"Yes"<<endl;
+	}
+	else
+	{
+		cout<<"No"<<endl;
+	}
+}
+}
+
+
+
+
